572d83042b0889dfda06dc1949e4a50263e02579
Matheus Queiroz <matheus_giga@hotmail.com>
2022-06-21 16:51:40 -0300

    Mais um alteração requisito 1

---
+++ src/components/SolarSystem.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class SolarSystem extends React.Component {
+  render() {
+    return (
+      <div>
+        <p>Solar System</p>
+      </div>
+    );
+  }
+}
+
+export default SolarSystem;
